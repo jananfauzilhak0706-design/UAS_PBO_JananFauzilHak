739d930c57a22ac67004d78c8e842e598b29e5b0
jananfauzilhak0706-design <jananfauzilhak0706@gmail.com>
2026-06-25 15:49:12 +0700

    tahap5 mahasiswabidikmisi

--- MahasiswaBidikmisi.php
+++ MahasiswaBidikmisi.php
@@ -1,26 +1,16 @@
 <?php
-// Panggil berkas induk
 require_once 'Mahasiswa.php';
 
-// SUBCLASS: MahasiswaBidikmisi
 class MahasiswaBidikmisi extends Mahasiswa {
-    // Properti Tambahan Spesifik Anak
     private $nomorKipKuliah;
     private $danaSakuSubsidi;
 
     public function __construct($dataRow) {
         parent::__construct($dataRow);
-        
-        // Pemetaan atribut spesifik dari kolom database
         $this->nomorKipKuliah  = isset($dataRow['nomor_kip_kuliah']) ? $dataRow['nomor_kip_kuliah'] : '-';
         $this->danaSakuSubsidi = isset($dataRow['dana_saku_subsidi']) ? $dataRow['dana_saku_subsidi'] : 0;
     }
 
-    /**
-     * Metode Query Spesifik untuk Mahasiswa Jalur Bidikmisi
-     * @param mysqli $db - Objek koneksi database
-     * @return array - Kumpulan objek MahasiswaBidikmisi
-     */
     public static function getDaftarBidikmisi($db) {
         $query = "SELECT * FROM tabel_mahasiswa WHERE jenis_pembiayaan = 'Bidikmisi'";
         $result = $db->query($query);
@@ -34,12 +24,13 @@ class MahasiswaBidikmisi extends Mahasiswa {
         return $kumpulanObjek;
     }
 
-    // Mengimplementasikan metode abstrak dari kelas induk
+    /**
+     * [Tahap 5] Overriding: Menghitung total tagihan Mahasiswa Bidikmisi
+     */
     public function hitungTagihanSemester() {
         return 0;
     }
 
-    // Mengimplementasikan metode abstrak dari kelas induk
     public function tampilkanSpesifikasiAkademik() {
         echo "<strong>— Detail Skema Bidikmisi —</strong><br>";
         echo "No. KIP Kuliah   : " . $this->nomorKipKuliah . "<br>";
