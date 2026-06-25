5a2f0b6e46ae47946f77281caec4a4cd85492038
jananfauzilhak0706-design <jananfauzilhak0706@gmail.com>
2026-06-25 15:48:25 +0700

    tahap5 mahasiswamandiri

--- MahasiswaMandiri.php
+++ MahasiswaMandiri.php
@@ -1,27 +1,16 @@
 <?php
-// Panggil berkas induk
 require_once 'Mahasiswa.php';
 
-// SUBCLASS: MahasiswaMandiri
 class MahasiswaMandiri extends Mahasiswa {
-    // Properti Tambahan Spesifik Anak
     private $golonganUkt;
     private $namaWali;
 
     public function __construct($dataRow) {
-        // Meneruskan data global ke constructor class induk (Mahasiswa)
         parent::__construct($dataRow);
-        
-        // Pemetaan atribut spesifik dari kolom database
         $this->golonganUkt = isset($dataRow['golongan_ukt']) ? $dataRow['golongan_ukt'] : 0;
         $this->namaWali    = isset($dataRow['nama_wali']) ? $dataRow['nama_wali'] : '-';
     }
 
-    /**
-     * Metode Query Spesifik untuk Mahasiswa Jalur Mandiri
-     * @param mysqli $db - Objek koneksi database
-     * @return array - Kumpulan objek MahasiswaMandiri
-     */
     public static function getDaftarMandiri($db) {
         $query = "SELECT * FROM tabel_mahasiswa WHERE jenis_pembiayaan = 'Mandiri'";
         $result = $db->query($query);
@@ -29,19 +18,19 @@ class MahasiswaMandiri extends Mahasiswa {
         $kumpulanObjek = [];
         if ($result && $result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-                // Mengonversi baris tabel menjadi objek dari kelas ini sendiri
                 $kumpulanObjek[] = new self($row);
             }
         }
         return $kumpulanObjek;
     }
 
-    // Mengimplementasikan metode abstrak dari kelas induk (sementara return default)
+    /**
+     * [Tahap 5] Overriding: Menghitung total tagihan Mahasiswa Mandiri
+     */
     public function hitungTagihanSemester() {
-        return $this->tarifUktNominal;
+        return $this->tarifUktNominal + 100000;
     }
 
-    // Mengimplementasikan metode abstrak dari kelas induk
     public function tampilkanSpesifikasiAkademik() {
         echo "<strong>— Detail Skema Mandiri —</strong><br>";
         echo "Golongan UKT: " . $this->golonganUkt . "<br>";
